ddc77ed9c36d88d449d59de59a8d32a97ede0b0c
tuanapa <tuan.le@napaglobal.com>
2023-09-28 10:44:51 +0700

    change

--- src/App.js
+++ src/App.js
@@ -6,9 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.sssss
-        </p>
+        <p>This is something new</p>
         <a
           className="App-link"
           href="https://reactjs.org"
